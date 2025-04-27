fa8b845306e70400c8dce8ee563c796ed8164ce8
Gaurav Gupta <gouravgupta840@gmail.com>
2025-04-27 17:54:46 +0000

    [skip ci] added tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Ensure it includes all subdirectories
+  darkMode: false, // 'media' or 'class' based on preference
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
