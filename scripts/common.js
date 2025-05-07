845bb0a93a234ac6b9cb314d1f052b7d1df1780e
Gaurav Gupta <gouravgupta840@gmail.com>
2025-05-07 14:32:46 +0530

    [skip ci] Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -1,1 +1,14 @@
+function isLocalhostOrHttp() {
+    const hostname = window.location.hostname;
+    const protocol = window.location.protocol;
 
+    const isLocalhost = hostname == 'localhost' || hostname == '127.0.0.1';
+    const isHttp = protocol == 'http:';
+
+    return isLocalhost || isHttp;
+}
+
+function getHost() {
+    // const local = `${window.location.host}//${window.location.host}`
+    return isLocalhostOrHttp() ? `http://localhost:12000` : `https://example.com`;
+}
