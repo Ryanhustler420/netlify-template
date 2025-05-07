9919385c0465a751255596f552f7bf55569f5560
Gaurav Gupta <gouravgupta840@gmail.com>
2025-05-07 14:36:00 +0530

    [skip ci] Update routes.js

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,1 +1,8 @@
-
+async function fake() {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+    console.log(response.data);
+  } catch (error) {
+    console.error(error.response.data);
+  }
+}
